7b77d20a5ee02a1828a8235bbf994e61693f6fec
Kyle Robinson Young <kyle@dontkry.com>
2014-12-05 23:33:45 -0800

    Ability to purchase items

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 var raf = require('raf')
 var vkey = require('vkey')
 var tic = require('./lib/common').tic
+var rand = require('./lib/common').rand
 var world = require('./lib/world')()
 
 // CREATE PLAYER
 var player = world.spawnEntity('player', [0,0])
 
+// CREATE STORE
+var store = world.spawnEntityRandom('store', [0,0], [50,50])
+world.onbuy = function(item) {
+  store.buy(player, item)
+}
+
 // SPAWN SNOWFLAKES AROUND PLAYER
 tic.interval(function spawnSnowflake() {
-  world.spawnEntityRandom('snowflake', player.position, [50,50])
+  world.spawnEntityRandom('snowflake', player.position, [100,100])
 }, 5000)
-world.spawnEntityRandom('snowflake', player.position, [50,50])
-world.spawnEntityRandom('snowflake', player.position, [50,50])
-world.spawnEntityRandom('snowflake', player.position, [50,50])
-world.spawnEntityRandom('snowflake', player.position, [50,50])
+for (var i = 0; i < 10; i++) {
+  var sf = world.spawnEntityRandom('snowflake', player.position, [100,100])
+  sf.lifespan = rand(10000, 60000)
+}
 
-// UPDATE MENU BAR
+// UPDATE MENU BARS
 tic.interval(function updateMenu() {
   document.getElementById('menu-hp').innerHTML = 'HP: ' + player.hp
-  document.getElementById('menu-snowflakes').innerHTML = 'SNOWFLAKES: ' + player.inv.snowflakes
-}, 500)
+  document.getElementById('menu-snowflakes').innerHTML = '❄: ' + player.inv.snowflakes
+  document.getElementById('menu-str').innerHTML = 'STR: ' + player.str
+  document.getElementById('menu-def').innerHTML = 'DEF: ' + player.def
+  document.getElementById('store').style.display = (player.canShop) ? 'block' : 'none'
+}, 200)
 
 // KEYBOARD CONTROLS
 window.onkeydown = function keyDown(e) {
